77c16dfb4f4b49ac576327c9912db934dffcaa76
Paulo Moura <pmoura@logtalk.org>
2019-10-24 12:17:46 +0100

    Experiment with exporting LOGTALKHOME and LOGTALKUSER from action

--- src/installer.js
+++ src/installer.js
@@ -1,3 +1,4 @@
+const core = require('@actions/core')
 const {exec} = require('@actions/exec')
 const path = require('path')
 
@@ -11,8 +12,12 @@ module.exports = {installLogtalk}
 async function installLogtalk(version,dependencies) {
   if (process.platform == 'darwin') {
     await exec(path.join(__dirname, 'install-logtalk-darwin'), [version,dependencies])
+		core.exportVariable('LOGTALKHOME', '/usr/local/share/logtalk');
+		core.exportVariable('LOGTALKUSER', '$HOME/logtalk');
   } else if (process.platform == 'linux') {
     await exec(path.join(__dirname, 'install-logtalk-ubuntu'), [version,dependencies])
+		core.exportVariable('LOGTALKHOME', '/usr/local/share/logtalk');
+		core.exportVariable('LOGTALKUSER', '$HOME/logtalk');
   } else if (process.platform == 'win32') {
     await exec(path.join(__dirname, 'install-logtalk-windows'), [version,dependencies])
   }
