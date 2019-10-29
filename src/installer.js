dbd7ca53f96c0380fafffae30fb2fbbed51f9ae6
Paulo Moura <pmoura@logtalk.org>
2019-10-29 11:13:24 +0000

    Skip cpan setup prompts

--- src/installer.js
+++ src/installer.js
@@ -12,12 +12,12 @@ module.exports = {installLogtalk}
 async function installLogtalk(version,dependencies) {
   if (process.platform == 'darwin') {
     await exec(path.join(__dirname, 'install-logtalk-darwin'), [version,dependencies])
-		core.exportVariable('LOGTALKHOME', '/usr/local/share/logtalk');
-		core.exportVariable('LOGTALKUSER', '/Users/runner/logtalk');
+    core.exportVariable('LOGTALKHOME', '/usr/local/share/logtalk');
+    core.exportVariable('LOGTALKUSER', '/Users/runner/logtalk');
   } else if (process.platform == 'linux') {
     await exec(path.join(__dirname, 'install-logtalk-ubuntu'), [version,dependencies])
-		core.exportVariable('LOGTALKHOME', '/usr/local/share/logtalk');
-		core.exportVariable('LOGTALKUSER', '/home/runner/logtalk');
+    core.exportVariable('LOGTALKHOME', '/usr/local/share/logtalk');
+    core.exportVariable('LOGTALKUSER', '/home/runner/logtalk');
   } else if (process.platform == 'win32') {
     await exec(path.join(__dirname, 'install-logtalk-windows'), [version,dependencies])
   }
